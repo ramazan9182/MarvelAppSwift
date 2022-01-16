268eac9d4d7a564685c8bc23711310f0cbd67deb
Ramazan Ceylan <Ramazan.CEYLAN@halkbank.com.tr>
2022-01-16 20:49:40 +0300

    Initial Commit

---
+++ MarvelAPISwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelAPISwift
+//
+//  Created by Ramazan Ceylan on 16.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
